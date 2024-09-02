ade4fb2cc1dcf812ee0be0fc1c6faa9604391605
GD-aish0o <aqsaakqaazi@gmail.com>
2024-09-03 02:47:30 +0500

    setup a project repo.

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className="wrapper">
+      <h1>Home</h1>
+      <p>Some content</p>
+
+    </div>
+ );
+}
+
+// so this is react..!
+// louder ahn basics j
+
+
+
+
+
+// when did i init git repo?
+// how do i push it on my repo?
+// syntax se lg rha js ka function
+
+//  i did ctrl = basics
