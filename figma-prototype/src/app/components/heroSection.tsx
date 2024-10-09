282a1f68348b869174180d01a3c89e84b5496da3
GD-aish0o <aqsaakqaazi@gmail.com>
2024-10-09 23:58:03 +0500

    Fixed responsiveness

--- figma-prototype/src/app/components/heroSection.tsx
+++ figma-prototype/src/app/components/heroSection.tsx
@@ -8,26 +8,26 @@ export default function Hero() {
                 width: '100%',
                 height: '100vh',
             }}
-            className="h-4/5 w-full flex flex-col justify-center items-center"
+            className="w-full h-screen flex flex-col justify-center items-center"
         >
             {/* content */}
-            <div className="justify-start text-left break-words w-full md:w-2/3 lg:w-1/3 px-4 md:px-0 lg:-ml-80">
-                <h1 className="text-4xl md:text-5xl lg:text-6xl font-black mb-1">
-                    FIND CLOTHES THAT MATCHES YOUR STYLE
+            <div className="text-left w-full px-6 sm:px-8 md:w-2/3 lg:w-1/2 xl:w-1/3">
+                <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-black mb-4">
+                    FIND CLOTHES THAT MATCH YOUR STYLE
                 </h1>
-                <p className="font-light text-black text-opacity-60 mt-2 text-sm md:text-base lg:text-lg">
+                <p className="font-light text-black text-opacity-60 text-sm sm:text-base md:text-lg mt-4">
                     Browse through our diverse collection of clothing and find the perfect outfit for you. Explore now and discover the latest trends in fashion.
                 </p>
 
                 <button
                     type="button"
                     title="shop"
-                    className="text-white font-light text-sm md:text-md lg:text-lg px-6 md:px-7 lg:px-9 py-2 md:py-3 bg-black rounded-full cursor-pointer mt-5"
+                    className="text-white font-light text-sm sm:text-md px-6 sm:px-8 py-2 sm:py-3 bg-black rounded-full cursor-pointer mt-6"
                 >
                     Shop Now
                 </button>
             </div>
-            <div></div>
         </main>
     );
 }
+
